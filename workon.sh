ad807b4a15037a5814afc0be82b68c94bfac93cb
Adith pp <ppadith93@gmail.com>
2023-08-17 02:43:42 +0530

    Do shell scripting faster

--- workon.sh
+++ workon.sh
@@ -1,15 +1,32 @@
 # shell-script-to-do-shell-scripting-fast
-# https://www.theunixschool.com/2011/08/shell-script-to-do-shell-scripting.html
+# Based on https://www.theunixschool.com/2011/08/shell-script-to-do-shell-scripting.html
+
 #!/usr/bin/bash
 
-if [ ! -f $1 ]; then
-  echo "#!/usr/bin/bash" > $1
+# check to ensure that the correct number of command-line arguments is provided.
+# If not, the script displays the correct usage and exits.
+if [ $# -ne 1 ]; then
+# '$#' is a special variable in Bash that represents the number of commandline
+# arguments passed to the script.
+  echo "Usage: $0 <script_filename.sh>"
+  exit 1
 fi
 
-while [ 1 ];
+# Check if file is present
+if [ ! -f "$PWD/$1" ]; then
+    echo -e "# A Program for\n\n#!/usr/bin/bash\n\n# Set the shell option to exit immediately if any command exits with a non-zero status\nset -e" > ""$1""
+fi
+
+# Check if file is executable
+if [ ! -x "$PWD/$1" ]; then
+  chmod +x "$PWD/$1"
+fi
+
+while true;
 do
-        nano $1
-        chmod 755 $1
-        ./$1
-        read dummy
+    nano "$PWD/$1"
+    "$PWD/$1"
+    read -rp "Press Enter to continue..."
+    # wait for you to press the Enter key before looping again 
+    # and opening the script for editing
 done
