f05342b4186a9f3f3eee05be481cea4db6bfbbc7
Adith pp <ppadith93@gmail.com>
2023-08-17 01:43:07 +0530

    notification sound only on battery lvl change

--- battery_status.sh
+++ battery_status.sh
@@ -1,20 +1,31 @@
 # Provide notification when battery level goes below 20% and above 80%
 
 #!/bin/bash
+prev_batt=0
+
+function play_notification_sound() {
+    prev_batt=$1    # Update the previous battery level
+    paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+}
+
 while true; do
-    export DISPLAY=:0.0
-    battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)')
-    ac_power=$(cat /sys/class/power_supply/AC/online)
+    export DISPLAY=:0.0 # Set the display for notifications to :0.0 (default display)
+    battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)') # Extract the battery level
+    ac_power=$(cat /sys/class/power_supply/AC/online)   # Check AC power status
 
     if [[ $ac_power == "1" ]]; then
         if [[ $battery_level -ge 82 ]]; then
             notify-send "Battery Full" "Level: ${battery_level}%"
-            paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+            if [[ $prev_batt -ne $battery_level ]]; then
+                play_notification_sound $battery_level
+            fi
         fi
     else
         if [[ $battery_level -le 20 ]]; then
             notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}%"
-            paplay /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+            if [[ $prev_batt -ne $battery_level ]]; then
+                play_notification_sound $battery_level
+            fi
         fi
     fi
 
