d293b482ba2b3c1bdb47dae7726be49afc77deec
Adith pp <136182506+a-dith@users.noreply.github.com>
2024-04-02 12:11:53 +0530

    Update process.sh
    added option to move, delete or ignore previously played file

--- process.sh
+++ process.sh
@@ -2,7 +2,7 @@
 
 #!/bin/bash
 set -e
-
+previous_played_file=""
 folder_name="$HOME/Downloads/"
 dmenu_options="-i -l 20 -nb black -nf white -sb dodgerblue -sf white -fn 'Monospace-12'"
 
@@ -18,6 +18,34 @@ select_destination_folder() {
 }
 
 while true; do
+
+         # Operations for the previously played file if it exists
+         if [[ -n "$previous_played_file" ]]; then
+             previous_played_file_option=("Move" "Delete" "ignore")
+             selected_previous_played_file_option=$(printf '%s\n' "${previous_played_file_option[@]}" | dmenu -p "${previous_played_file}" $dmenu_options)
+
+            case "$selected_previous_played_file_option" in
+                        "Move")
+                            destination_folder=$(select_destination_folder)
+                            if [[ -n "$destination_folder" ]]; then
+                                mv "$folder_name$previous_played_file" "$destination_folder"
+                            else
+                                echo "Invalid destination folder. File not moved."
+                            fi
+                            previous_played_file=""
+                           ;;
+                        "Delete")
+                            # Add code to delete the selected file
+                            rm "$folder_name$previous_played_file"
+                            previous_played_file=""
+                            ;;
+                        "ignore")
+                            previous_played_file=""
+                            # User chose to do nothing
+                            ;;
+                    esac
+         fi
+
     result="$(ls -t "$folder_name" | { echo '..'; cat; } | dmenu -p "$folder_name" $dmenu_options)"
 
     if [[ "$result" == ".." ]]; then
@@ -32,12 +60,13 @@ while true; do
         case "$selected_option" in
             "Play")
                 # Add code to play the selected file
-                mpv "$folder_name$result"
+                mpv --speed=2 "$folder_name$result"
+                previous_played_file="$result"
                 ;;
             "Copy name")
                  # Add code to copy the selected file name to clipboard
                  echo "$result" | xclip -sel c
-                 break
+                 previous_played_file=""
                  ;;
             "Move")
                 destination_folder=$(select_destination_folder)
@@ -46,10 +75,12 @@ while true; do
                 else
                     echo "Invalid destination folder. File not moved."
                 fi
+                previous_played_file=""
                 ;;
             "Delete")
                 # Add code to delete the selected file
                 rm "$folder_name$result"
+                previous_played_file=""
                 ;;
             "Cancel")
                 # User chose to cancel, break out of the loop
