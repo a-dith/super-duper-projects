4d56001d60b20f6cc9c0ceb8a2d9210844a01ec9
Adith pp <136182506+a-dith@users.noreply.github.com>
2024-04-07 17:44:42 +0530

    Update setup_and_install_packages.sh

--- setup_and_install_packages.sh
+++ setup_and_install_packages.sh
@@ -31,6 +31,15 @@ done
 
 echo "Package installation completed."
 
+cd ~
+# installing xtreme download manager
+echo "installing xtreme download manager..." && wget https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz && tar xf xdm-setup-7.2.11.tar.xz && chmod a+x ./install.sh && su -c ./install.sh 
+rm xdm-se*.tar.xz readme.txt install.sh
+
+# install appimage manager
+wget https://raw.githubusercontent.com/ivan-hc/AM/main/INSTALL && chmod a+x ./INSTALL && sudo ./INSTALL
+
+# it is getting downloaded in the root folder so find a war to change it
 # installing piper tts
 cd ~
 echo "installing piper TTS..."
@@ -45,14 +54,6 @@ wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_US/lessac/
 # remove the compressed file
 rm ~/piper*.gz
 
-cd ~
-# installing xtreme download manager
-echo "installing xtreme download manager..." && wget https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz && tar xf xdm-setup-7.2.11.tar.xz && chmod a+x ./install.sh && su -c ./install.sh 
-rm xdm-se*.tar.xz readme.txt install.sh
-
-# install appimage manager
-wget https://raw.githubusercontent.com/ivan-hc/AM/main/INSTALL && chmod a+x ./INSTALL && sudo ./INSTALL
-
 # firefox dark reader
 firefox https://darkreader.org/
 # firefox OLED theme (dark)
@@ -60,7 +61,7 @@ firefox "https://addons.mozilla.org/en-US/firefox/addon/oled-with-red-accent/?ut
 # online keyboard tester
 firefox "https://shawon9324.github.io/apps/keytester/"
 
-keyboard shortcuts
+# keyboard shortcuts
 # win + q  => clip board
 xfconf-query -c xfce4-keyboard-shortcuts -p  '/commands/custom/<Super>q' -n -t string -s 'xfce4-clipman-history'
 # win + t  => terminal
@@ -70,4 +71,5 @@ xfconf-query -c xfce4-panel -p /panels/panel-0/autohide-behavior -t uint -s 2
 # add taskmanager to ctrl+shift+esc 
 xfconf-query -c xfce4-keyboard-shortcuts -p  '/commands/custom/<Primary><Shift>Escape' -n -t string -s 'xfce4-taskmanager'
 # set terminal transparency
-xfconf-query --channel xfwm4 -p /general/popup_opacity -s 65
+
+
